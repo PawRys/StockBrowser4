4bf05f9edb57ad80590ae25a0b6f4f8da42baf5d
PawRys <hellbu@gmail.com>
2023-09-22 19:09:55 +0200

    rebuild main app view

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,29 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { useCurrentTabStore } from '@/stores/currentTabStore'
+
+import ProductsList from './components/tabs/ProductsList.vue'
+import DatabaseShare from './components/tabs/DatabaseShare.vue'
+import DatabaseUpdate from './components/tabs/DatabaseUpdate.vue'
+import DatabaseManager from './components/tabs/DatabaseManager.vue'
+
+const tabs = [
+  { comp: ProductsList, name: 'ProductsList', label: 'Produkty' },
+  { comp: DatabaseShare, name: 'DatabaseShare', label: 'Załaduj dane' },
+  { comp: DatabaseUpdate, name: 'DatabaseUpdate', label: 'DatabaseUpdate' },
+  { comp: DatabaseManager, name: 'DatabaseManager', label: 'DatabaseManager' }
+]
+const currentTab = useCurrentTabStore()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1>Stock Browser 4</h1>
+  <div>
+    <button v-for="(tab, index) in tabs" :key="index" @click="currentTab.tabId = index">
+      {{ tab.label }}
+    </button>
+  </div>
+  <component :is="tabs[currentTab.tabId].comp" class="tab"></component>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
+@/stores/appTabStore @/stores/currentTabStore
