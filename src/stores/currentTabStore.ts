0d516f8e626f9a077e2549a0df7a3e374befcc50
PawRys <hellbu@gmail.com>
2023-09-22 19:08:32 +0200

    created store for tab switching

---
+++ src/stores/currentTabStore.ts
@@ -0,0 +1,12 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCurrentTabStore = defineStore(
+  'currentTab',
+  () => {
+    const tabId = ref(0)
+
+    return { tabId }
+  },
+  { persist: true }
+)
