18e531ab1b803064708de97fd7a652c4b2a49da6
PawRys <hellbu@gmail.com>
2023-11-04 14:46:05 +0100

    cosmetics

--- src/stores/paginationStore.ts
+++ src/stores/paginationStore.ts
@@ -13,16 +13,22 @@ export const usePageStore = defineStore(
     const pageCount = computed(() => Math.ceil(products.value.length / pageSize.value))
     const currentPageFactor = computed(() => currentPage.value * pageSize.value - pageSize.value)
     const nextPage = () => {
-      if (currentPage.value < pageCount.value) currentPage.value++
+      if (currentPage.value < pageCount.value) {
+        currentPage.value++
+      }
     }
     const prevPage = () => {
-      if (currentPage.value > 1) currentPage.value--
+      if (currentPage.value > 1) {
+        currentPage.value--
+      }
     }
     const setCurrentPage = (event: Event) => {
-      currentPage.value = Number((event.target as HTMLSelectElement).value)
+      const target = event.target as HTMLSelectElement
+      currentPage.value = Number(target.value)
     }
     const setPageSize = (event: Event) => {
-      pageSize.value = Number((event.target as HTMLSelectElement).value)
+      const target = event.target as HTMLSelectElement
+      pageSize.value = Number(target.value)
     }
     watch([products, pageSize], () => {
       if (currentPage.value > pageCount.value) {
