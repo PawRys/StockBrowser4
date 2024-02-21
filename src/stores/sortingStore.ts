0181de9d319d58d6dfc785ca01ba4044909773d2
PawRys-Smetek <hellbu@gmail.com>
2024-02-21 15:33:37 +0100

    wip

--- src/stores/sortingStore.ts
+++ src/stores/sortingStore.ts
@@ -7,7 +7,15 @@ export const useSortingStore = defineStore(
     const sortDir = ref(0)
     const sortUnit = ref('m3')
     const sortColumn = ref('id')
-    watch([sortUnit, sortColumn], () => (sortDir.value = 0))
+    watch(
+      [sortUnit, sortColumn],
+      () => {
+        sortDir.value = 0
+        console.log(sortColumn.value)
+      },
+      { immediate: true }
+    )
+    watch(sortDir, () => console.log(sortDir.value), { immediate: true })
 
     return { sortUnit, sortDir, sortColumn }
   },
--- src/stores/stocksStore.ts
+++ src/stores/stocksStore.ts
@@ -3,58 +3,72 @@ import { defineStore } from 'pinia'
 import { useFilterStore } from '@/stores/filterStore'
 import { useSortingStore } from '@/stores/sortingStore'
 import { calcPrice, calcQuant } from '../components/Utils/functions'
+// const filterStore = useFilterStore()
+// const sortingStore = useSortingStore()
 
 const collator = new Intl.Collator(undefined, {
   usage: 'sort',
   numeric: true
 })
 
+function applyStatusFilter(el: Plywood) {
+  const filterStore = useFilterStore()
+  return el.stockStatus >= filterStore.status_filter
+}
+
+function applyTextFilter(el: Plywood) {
+  const filterStore = useFilterStore()
+  return `${el.id} ${el.name}`.match(new RegExp(filterStore.text_filter, 'gi'))
+}
+
+function applyTagFilter(el: Plywood) {
+  const filterStore = useFilterStore()
+  let count = 0
+  const tagFilterKeys = Object.keys(filterStore.tag_filter)
+  for (const key of tagFilterKeys) {
+    const k = key as keyof typeof filterStore.tag_filter
+    const tagFilterProxy = filterStore.tag_filter[k]
+    const tagFilterArr = JSON.parse(JSON.stringify(tagFilterProxy))
+    const plywoodAttrArr = el.attr[k].split(' ')
+    const set = [...new Set([...tagFilterArr, ...plywoodAttrArr])]
+    if (tagFilterArr.length + plywoodAttrArr.length > set.length) {
+      count++
+    }
+  }
+  return count == tagFilterKeys.length ? true : false
+}
+
+function applySorting(a: any, b: any) {
+  const sortingStore = useSortingStore()
+  const sortColumn = sortingStore.sortColumn
+  const sortUnit = sortingStore.sortUnit
+
+  let first = a[sortColumn]
+  let last = b[sortColumn]
+
+  if (sortColumn.match(/price/i)) {
+    first = calcPrice(a.size, a[sortColumn], sortUnit, 'm3')
+    last = calcPrice(b.size, b[sortColumn], sortUnit, 'm3')
+  }
+  if (sortColumn.match(/Stock/i)) {
+    first = calcQuant(a.size, a[sortColumn], sortUnit, 'm3')
+    last = calcQuant(b.size, b[sortColumn], sortUnit, 'm3')
+  }
+  if (sortingStore.sortDir !== 1) return collator.compare(first, last)
+  if (sortingStore.sortDir === 1) return collator.compare(last, first)
+  return collator.compare(first, last)
+}
+
 export const useStocksStore = defineStore(
   'SB4_stocksStore',
   () => {
-    const filterStore = useFilterStore()
-    const sortingStore = useSortingStore()
     const products = computed(() => {
       if (!localStorage.SB4_products) return []
       return JSON.parse(localStorage.SB4_products)
-        .filter((el: Plywood) => el.stockStatus >= filterStore.status_filter)
-        .filter((el: Plywood) => {
-          let count = 0
-          const tagFilterKeys = Object.keys(filterStore.tag_filter)
-          for (const key of tagFilterKeys) {
-            const k = key as keyof typeof filterStore.tag_filter
-            const tagFilterProxy = filterStore.tag_filter[k]
-            const tagFilterArr = JSON.parse(JSON.stringify(tagFilterProxy))
-            const plywoodAttrArr = el.attr[k].split(' ')
-            const set = [...new Set([...tagFilterArr, ...plywoodAttrArr])]
-            if (tagFilterArr.length + plywoodAttrArr.length > set.length) {
-              count++
-            }
-          }
-
-          return count == tagFilterKeys.length ? true : false
-        })
-        .filter((el: Plywood) =>
-          `${el.id} ${el.name}`.match(new RegExp(filterStore.text_filter, 'gi'))
-        )
-        .sort((a: any, b: any) => {
-          const sortColumn = sortingStore.sortColumn
-          const sortUnit = sortingStore.sortUnit
-
-          let first = a[sortColumn]
-          let last = b[sortColumn]
-
-          if (sortColumn.match(/price/i)) {
-            first = calcPrice(a.size, a[sortColumn], sortUnit, 'm3')
-            last = calcPrice(b.size, b[sortColumn], sortUnit, 'm3')
-          }
-          if (sortColumn.match(/Stock/i)) {
-            first = calcQuant(a.size, a[sortColumn], sortUnit, 'm3')
-            last = calcQuant(b.size, b[sortColumn], sortUnit, 'm3')
-          }
-          if (sortingStore.sortDir !== 1) return collator.compare(first, last)
-          if (sortingStore.sortDir === 1) return collator.compare(last, first)
-        })
+        .filter((el: Plywood) => applyStatusFilter(el))
+        .filter((el: Plywood) => applyTagFilter(el))
+        .filter((el: Plywood) => applyTextFilter(el))
+        .sort(applySorting)
     })
 
     // watch(
